c3148c64a2e69c70b586082f4c24d56b57756d7a
Runchi <runchidasemail@gmail.com>
2022-03-07 09:45:55 +0100

    - initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Would you rather
+      </header>
+      Hello!
+    </div>
+  );
+}
+
+export default App;
