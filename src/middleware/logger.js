3b69739f4eac8e44b1572091887958441a2f29b4
Runchi <runchidasemail@gmail.com>
2022-03-10 17:56:42 +0100

    - fix logger

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -2,7 +2,7 @@ const logger = (store) => (next) => (action) => {
     console.group(action.type)
     console.log('Action: ', action)
     const returnValue = next(action)
-    console.log('New State: ', store.returnValue)
+    console.log('New State: ', returnValue)
     console.groupEnd()
     return returnValue
 }
--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -5,7 +5,7 @@ export default function questions(state = {}, action) {
         case RECEIVE_QUESTIONS:
             return {
                 ...state,
-                ...action.questions
+                ...(action.questions)
             }
         default:
             return state
